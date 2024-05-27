887cb168bd7fa1c4e3f30786541306758f27742f
korzhix <korzhstud@gmail.com>
2024-05-27 22:33:52 +0300

    Add config

---
+++ src/config.py
@@ -0,0 +1,14 @@
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    REDIS_URL: str
+    model_config = SettingsConfigDict(env_file="src/.env",
+                                      env_file_encoding='utf-8')
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
--- src/contact/redis_client.py
+++ src/contact/redis_client.py
@@ -1,10 +1,12 @@
 import redis.asyncio as redis
 
-REDIS_URL = "redis://localhost"
+from fastapi import Depends
+from typing import Annotated
+from src import config
 
 
-async def get_redis_connection():
-    pool = redis.ConnectionPool.from_url(REDIS_URL)
+async def get_redis_connection(settings: Annotated[config.Settings, Depends(config.get_settings)]) -> redis.Redis:
+    pool = redis.ConnectionPool.from_url(settings.REDIS_URL)
     client = redis.Redis.from_pool(pool)
     yield client
     await client.close()
